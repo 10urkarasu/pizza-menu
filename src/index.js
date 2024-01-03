6197c72348f835dd56f6e65afca1fabdca677d38
10urkarasu <10urkarasu@gmail.com>
2024-01-04 02:23:46 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+  return (
+    <>
+      <h1>Hello React</h1>
+    </>
+  );
+}
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
